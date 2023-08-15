311c1b05f1f6bcb5415f92d59b8bb0be8ce421c8
agnes cameron <agnesfcameron@gmail.com>
2023-08-15 14:46:45 +0200

    network generation working

--- components.py
+++ components.py
@@ -1,4 +1,5 @@
 from materials import *
+import copy
 
 class Connection():
 	def __init__(self, source, dest):
@@ -8,13 +9,12 @@ class Connection():
 	def draw(self, machines):
 		# check if there's space on the input of source
 		# and on the output of dest. if not then
-		print('drawing', self.source, self.dest)
 
 		machine = next((m for m in machines if m.machine_id == self.source), None)
 		if machine is not None:
 			in_idx = machine.outputs.index(False)
 			machine.outputs[in_idx] = self.dest
-			print('output added to', machine.name, machine.machine_id, 'dest is', self.dest)
+			print('connected', machine.name, (machine.machine_id), 'to', self.dest)
 		else:
 			print("couldn't find source machine")
 
@@ -24,24 +24,27 @@ class Connection():
 		print('deleting', self.source, self.dest)
 
 class Machine:
-	def __init__(self, machine_id, name):
-		self.inputs = [False]
-		self.name = name
+	def __init__(self):
+
+		self.outflow = []
 
 class heater(Machine):
 	def __init__(self, machine_id, name):
+		super().__init__()
 		self.inputs = [False]
 		self.name = name
 		self.machine_id = machine_id
 		self.outputs = [False]
 
 	def process(self):
-		material = self.inputs[0]['material']
+		material = copy.deepcopy(self.inputs[0]['material'])
 		material.name = "hot " + material.name
+		self.outflow =  [{'material': material, 'amount': self.inputs[0]['amount']}]
 		return [{'material': material, 'amount': self.inputs[0]['amount']}]
 
 class dryer(Machine):
 	def __init__(self, machine_id, name):
+		super().__init__()
 		self.inputs = [False]
 		self.name = name
 		self.machine_id = machine_id
@@ -52,22 +55,27 @@ class dryer(Machine):
 		if getattr(material, "dry", None) is not None:
 			material = material.dry()
 		else: print('cannot dry')
+		self.outflow = [{'material': material, 'amount': self.inputs[0]['amount']}]
 		return [{'material': material, 'amount': self.inputs[0]['amount']}]
 
 class split_gate(Machine):
 	def __init__(self, machine_id, name):
+		super().__init__()
 		self.inputs = [False]
 		self.name = name
 		self.machine_id = machine_id
 		self.outputs = [False, False]
 
 	def process(self):
+		self.outflow = [{ 'amount': self.inputs[0]['amount']/2, 'material': self.inputs[0]['material']}, 
+		{ 'amount': self.inputs[0]['amount']/2, 'material': self.inputs[0]['material']}]
 		return [{ 'amount': self.inputs[0]['amount']/2, 'material': self.inputs[0]['material']}, 
 		{ 'amount': self.inputs[0]['amount']/2, 'material': self.inputs[0]['material']}]
 
 
 class mixer(Machine):
 	def __init__(self, machine_id, name, recipes=recipes):
+		super().__init__()
 		self.inputs = [False, False]
 		self.name = name
 		self.machine_id = machine_id
@@ -82,24 +90,30 @@ class mixer(Machine):
 				result = recipe.result
 
 		if self.inputs[0]['amount'] > self.inputs[1]['amount']:
+			self.outflow = [{'material': result, 'amount': self.inputs[1]['amount']*2}]
 			return [{'material': result, 'amount': self.inputs[1]['amount']*2}]
 
 		else:
+			self.outflow = [{'material': result, 'amount': self.inputs[0]['amount']*2}]
 			return [{'material': result, 'amount': self.inputs[0]['amount']*2}]
 
 class core(Machine):
 	def __init__(self, machine_id, name):
+		super().__init__()
 		self.inputs = [False]
 		self.name = name
 		self.machine_id = machine_id
 		self.outputs = [False, False]
 
 	def process(self):
+		self.outflow = [{ 'material': Piss(), 'amount': self.inputs[0]['amount']*0.4}, 
+			{ 'material': Blood(), 'amount': self.inputs[0]['amount']*0.4}]
 		return [{ 'material': Piss(), 'amount': self.inputs[0]['amount']*0.4}, 
 			{ 'material': Blood(), 'amount': self.inputs[0]['amount']*0.4}]
 
 class combi_gate(Machine):
 	def __init__(self, machine_id, name):
+		super().__init__()
 		self.inputs = [False, False]
 		self.name = name
 		self.machine_id = machine_id
@@ -111,15 +125,18 @@ class combi_gate(Machine):
 			print('  -  ', out['amount'], out['material'].name)
 
 		print('to outlet')
+		self.outflow = self.inputs
 		return self.inputs
 
 
 class inlet(Machine):
 	def __init__(self, machine_id, name):
+		super().__init__()
 		self.inputs = [{'amount': 1.0, 'material': Pellet()}]
 		self.name = name
 		self.machine_id = machine_id
 		self.outputs = [False]
 
 	def process(self):
+		self.outflow = self.inputs
 		return self.inputs
--- network.py
+++ network.py
@@ -1,5 +1,7 @@
 from components import *
 from materials import *
+import components
+import os
 
 def initialise_machines_full():
 	machines = []
@@ -9,10 +11,10 @@ def initialise_machines_full():
 	machines.append(inlet(0, 'inlet'))
 	machines.append(split_gate(1, 'splitter'))
 	machines.append(core(2, 'you'))
-	machines.append(mixer(3, 'mixer', recipes))
+	machines.append(mixer(3, 'mixer'))
 	machines.append(dryer(4, 'dryer'))
 	machines.append(split_gate(5,'splitter'))
-	machines.append(mixer(6, 'mixer', recipes))
+	machines.append(mixer(6, 'mixer'))
 	machines.append(heater(7, 'heater'))
 	machines.append(combi_gate(8, 'combined flow pipe'))
 
@@ -34,49 +36,117 @@ def initialise_machines_full():
 
 	return machines, connections
 
+def print_state():
+	# print(machines)
+	os.system('clear')
+	print("#############################")
+	print("WELCOME TO THIS CURSED MACHINE")
+	print("#############################\n")
+	print("goal: 10 hot teeth, 20 sand\n")
+	print("current world state:\n")
 
-# def initialise_grid():
+	for machine in machines:
+		print(machine.name, "with id", machine.machine_id)
+		print(" - ", len(machine.inputs), "available inputs")
+		print(" - ", len(machine.outputs), "available outputs")
 
+		for i, inflow in enumerate(machine.inputs):
+			if inflow != False:
+				print("recieving", inflow["amount"], inflow["material"].name, "on input", i+1)
+
+		for i, output in enumerate(machine.outflow):
+			if output != False:
+				print("leaking", output["amount"], output["material"].name, "from output", i+1)
+
+		print("")
+
+	for connection in connections:
+		print("connection from", connection.source, "to", connection.dest)
+		print("")
+
+
+def initialise_grid():
+	machines = []
+
+	machines.append(inlet(str(len(machines)), 'inlet'))
+	machines.append(core(str(len(machines)), 'you'))
+
+	return machines
+
+def add_machine(machine_type):
+	print(machines)
+	machineClass = getattr(components, machine_type)
+	if machineClass is not None:
+		machines.append(machineClass(str(len(machines)), machine_type))
+	else:
+		print('no machine of this type avaible')
+
+
+def resolve_network():
+		resolved_nodes = []
+		counter = 0
+
+		while len(resolved_nodes) < len(machines):
+			for node in machines:
+				if False not in node.inputs and node.machine_id not in resolved_nodes:
+					resolved_nodes.append(node.machine_id)
+					outputs = node.process()
+
+					for i, rx in enumerate(node.outputs):
+						rx_node = next((x for x in machines if x.machine_id == rx), None)
+
+						if rx_node is not None:
+							try:
+								in_idx = rx_node.inputs.index(False)
+								rx_node.inputs[in_idx] = {
+										'amount': outputs[i]['amount'], 
+										'material': outputs[i]['material']
+									}
+
+								# print('sending', outputs[i]['amount'], outputs[i]['material'].name, 'from', node.name, 'to', rx_node.name)
+							except:
+								print('no available inlets for', node.machine_id)
+
+			# if we have to go over the network more than twice the size of the network
+			# then we know it's not solvable (is this true?!)
+			counter+=1
+
+			if counter == len(machines)*2:
+				print('no source/sink connection')
+				break
 
 if __name__ == '__main__':
-	machines, connections = initialise_machines_full()
+	# machines, connections = initialise_machines_full()
+	machines = initialise_grid()
+	connections = []
 
 	# each time a machine or connection is added
 	# go over the list of connections and 'redraw' the network
 	# recalculate input / output
 
-	resolved_nodes = []
+	while True:
+		resolve_network()
+		print_state()
 
-	counter = 0
-	while len(resolved_nodes) < len(machines):
-		for node in machines:
-			if False not in node.inputs and node.machine_id not in resolved_nodes:
-				resolved_nodes.append(node.machine_id)
-				outputs = node.process()
+		opt = input('to add a machine press [1]. to make a connection, press [2]. to run simulation, press [3] \n> ')
 
-				for i, rx in enumerate(node.outputs):
-					rx_node = next((x for x in machines if x.machine_id == rx), None)
+		if opt == "1":
+			new_machine = input('name of new machine \n> ')
+			add_machine(new_machine)
 
-					if rx_node is not None:
-						try:
-							in_idx = rx_node.inputs.index(False)
-							rx_node.inputs[in_idx] = {
-									'amount': outputs[i]['amount'], 
-									'material': outputs[i]['material']
-								}
+		elif opt == "2":
+			source = input('id of machine for source of connection \n> ')
+			dest = input('id of machine for destination of connection \n> ')
+			new_conn = Connection(source, dest)
+			new_conn.draw(machines)
 
-							print('sending', outputs[i]['amount'], outputs[i]['material'].name, 'from', node.name, 'to', rx_node.name)
-						except:
-							print('no available inlets')
-					else:
-						print('end of network')
+			connections.append(new_conn)
 
-		# if we have to go over the network more than twice the size of the network
-		# then we know it's not solvable (is this true?!)
-		counter+=1
-
-		if counter == len(machines)*2:
-			print('no source/sink connection')
+		elif opt == "3":
 			break
 
-			# print('got output from', node.machine_id, outputs)
+		else:
+			print("invalid input, try again")
+
+
+				# print('got output from', node.machine_id, outputs)
